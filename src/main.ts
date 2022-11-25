1ff00b61f2014c6f6d41412384a03b2ac952b715
felipe2640 <felipe_brito09@hotmail.com>
2022-11-25 16:04:07 -0300

    refactor(methods): Only used

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [process.env.CORSORIGIN],
-    methods: ['GET', 'POST', 'DELETE', 'PATCH', 'PUT'],
+    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
     credentials: true,
   });
 
